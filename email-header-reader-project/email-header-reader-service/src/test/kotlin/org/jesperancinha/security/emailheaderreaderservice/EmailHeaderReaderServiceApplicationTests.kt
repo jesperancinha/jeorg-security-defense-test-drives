c0413ab0986c97e5000e8b52d1947d48505a3ddf
Joao Esperancinha <jofisaes@gmail.com>
2022-10-19 23:14:28 +0200

    email-header-reader-project starts

---
+++ email-header-reader-project/email-header-reader-service/src/test/kotlin/org/jesperancinha/security/emailheaderreaderservice/EmailHeaderReaderServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jesperancinha.security.emailheaderreaderservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmailHeaderReaderServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
